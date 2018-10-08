3f82000b196dbb906a1b7eb098d59c82afd45dc7
Max Cherry <mxmCherry@gmail.com>
2018-10-08 21:42:14 +0300

    multi: rename private type as well (just to be consistent)

--- thread_safe_multi.go
+++ thread_safe_multi.go
@@ -4,26 +4,26 @@ import "sync"
 
 // MultiThreadSafe synchronizes access to Moving Average calculator.
 func MultiThreadSafe(mas MultiMA) MultiMA {
-	return &threadSafeMulti{
+	return &multiThreadSafe{
 		mas: mas,
 	}
 }
 
 // ----------------------------------------------------------------------------
 
-type threadSafeMulti struct {
+type multiThreadSafe struct {
 	mx  sync.RWMutex
 	mas MultiMA
 }
 
-func (a *threadSafeMulti) Add(v float64) []float64 {
+func (a *multiThreadSafe) Add(v float64) []float64 {
 	a.mx.Lock()
 	avgs := a.mas.Add(v)
 	a.mx.Unlock()
 	return avgs
 }
 
-func (a *threadSafeMulti) Avg() []float64 {
+func (a *multiThreadSafe) Avg() []float64 {
 	a.mx.RLock()
 	avgs := a.mas.Avg()
 	a.mx.RUnlock()
