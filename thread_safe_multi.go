7b3b194fbc0c7a19c9a6fbd8600365bacbf31abc
Max Cherry <mxmCherry@gmail.com>
2018-10-08 21:31:20 +0300

    multi: make ThreadSafeMultiMA wrapper private (it does not have to be public)

--- thread_safe_multi.go
+++ thread_safe_multi.go
@@ -4,26 +4,26 @@ import "sync"
 
 // ThreadSafeMulti synchronizes access to Moving Average calculator.
 func ThreadSafeMulti(multiMA MultiMA) MultiMA {
-	return &ThreadSafeMultiMA{
+	return &threadSafeMulti{
 		multiMA: multiMA,
 	}
 }
 
 // ----------------------------------------------------------------------------
 
-type ThreadSafeMultiMA struct {
+type threadSafeMulti struct {
 	mx      sync.RWMutex
 	multiMA MultiMA
 }
 
-func (a *ThreadSafeMultiMA) Add(v float64) []float64 {
+func (a *threadSafeMulti) Add(v float64) []float64 {
 	a.mx.Lock()
 	avgs := a.multiMA.Add(v)
 	a.mx.Unlock()
 	return avgs
 }
 
-func (a *ThreadSafeMultiMA) Avg() []float64 {
+func (a *threadSafeMulti) Avg() []float64 {
 	a.mx.RLock()
 	avgs := a.multiMA.Avg()
 	a.mx.RUnlock()
