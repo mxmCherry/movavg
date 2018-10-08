2e7f2dea8c8b7d824fbc2f457c0b6b6d18f128e5
Max Cherry <mxmCherry@gmail.com>
2018-10-08 21:57:08 +0300

    multi: fix test strings

--- set_test.go
+++ set_test.go
@@ -7,37 +7,10 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("SMASet", func() {
+var _ = Describe("Set", func() {
 	var _ = (*SMA)(nil)
 
-	It("should manage a set of Simple Moving Average calculators", func() {
-		subject := Set{
-			NewSMA(2),
-			NewSMA(3),
-			NewSMA(4),
-		}
-
-		// zero until values added:
-		Expect(subject.Avg()).To(Equal([]float64{0, 0, 0}))
-
-		// load initial value
-		Expect(subject.Add(2)).To(Equal([]float64{2, 2, 2}))
-		Expect(subject.Avg()).To(Equal([]float64{2, 2, 2}))
-
-		Expect(subject.Add(4)).To(Equal([]float64{3, 3, 3}))
-		Expect(subject.Avg()).To(Equal([]float64{3, 3, 3}))
-
-		Expect(subject.Add(6)).To(Equal([]float64{5, 4, 4}))
-		Expect(subject.Avg()).To(Equal([]float64{5, 4, 4}))
-
-		Expect(subject.Add(8)).To(Equal([]float64{7, 6, 5}))
-		Expect(subject.Avg()).To(Equal([]float64{7, 6, 5}))
-
-		Expect(subject.Add(10)).To(Equal([]float64{9, 8, 7}))
-		Expect(subject.Avg()).To(Equal([]float64{9, 8, 7}))
-	})
-
-	It("creates a multi from a given set of MAs", func() {
+	It("creates a MultiMA calculator from a given set of MAs", func() {
 		subject := Set{
 			NewSMA(2),
 			NewSMA(3),
