a03b4c6da3d6ed16f22cf0f7797761bc93c84868
Max Cherry <mxmCherry@gmail.com>
2018-10-08 21:44:29 +0300

    multi: OCD: rename param to make godoc align

--- set.go
+++ set.go
@@ -5,10 +5,10 @@ package movavg
 type Set []MA
 
 // Add recalculates Simple Moving Average values and returns them.
-func (mas Set) Add(value float64) (newAvgs []float64) {
+func (mas Set) Add(v float64) (newAvgs []float64) {
 	result := make([]float64, len(mas))
 	for i, ma := range mas {
-		result[i] = ma.Add(value)
+		result[i] = ma.Add(v)
 	}
 	return result
 }
